2509ac94d43f8ca4044965e2f92da804cd0b07da
Boris <ulanowicz_b@hotmail.com>
2020-11-17 19:31:33 -0800

    new branch who dis

--- script.js
+++ script.js
@@ -25,21 +25,34 @@ myApp.getJokes = $.ajax({
 $.when(myApp.getImages, myApp.getJokes)
     .then(function (image, joke) {
         const { url } = image[0].data[0].images.web;
-        console.log(url, joke[0].joke);
-        jokesArray.push(joke[0].joke)
+        // console.log(url, joke[0].joke);
+        jokesArray.push(joke[0].joke);
+        imagesArray.push(image.url);
+        appendContent(url);
     })
     .fail(function (imageError, jokeError) {
         console.log(imageError, jokeError);
     })
 
-
-const jokesArray = []
+const imagesArray = [];
+const jokesArray = [];
 
 // myApp.getJokes.then(function (jokes) {
 //     jokesArray.push(jokes.joke)
 // })
 
-$('#btn').on('click', function () {
-    let jokeToAppend = `<p>${jokesArray}</p>`
-    $('#quote-text').html(jokeToAppend)
-})
+// ---- I turned your event listener into a function, and I call the function above in $.when (line 31)
+const appendContent = function(imageLink) {
+  $('#btn').on('click', function () {
+
+    let pairToAppend = `
+    <div class = "imageJokeBox">
+      <p>${jokesArray}</p>
+    </div>
+    `;
+    $('.appendToHere').append(pairToAppend);
+    // ----- took the returned image and set it as the background image of div.image-container (had to set height and width for .image-container in _main.scss)
+    $('.imageJokeBox').css('background-image', `url(${imageLink})`)
+  })
+}
+
