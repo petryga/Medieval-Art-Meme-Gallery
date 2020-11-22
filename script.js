b4f4aabd5e8fcf5793a9b6af2d98ef041087c228
Boris <ulanowicz_b@hotmail.com>
2020-11-22 08:57:18 -0800

    reorganized JS, added counter

--- script.js
+++ script.js
@@ -5,24 +5,58 @@ myApp.imagesArray = [];
 myApp.jokesArray = [];
 myApp.userArtSelection;
 
-myApp.imagesUrl = `https://openaccess-api.clevelandart.org/api/artworks/`;
+myApp.imageUrl = `https://openaccess-api.clevelandart.org/api/artworks/`;
 myApp.jokesUrl = `https://icanhazdadjoke.com/search`;
 
 // GET IMAGES AJAX CALL
-myApp.getImages = function (style) {
+myApp.getImages = function () {
   let call = $.ajax({
-    url: myApp.imagesUrl,
+    url: myApp.imageUrl,
     method: 'GET',
     dataType: 'json',
     data: {
-      q: style,
+      q: 'portrait',
       has_image: 1,
-      department: 'Medieval Art',
+      // department: 'Medieval Art',
       type: 'Painting',
     }
   });
   return call;
 }
+
+myApp.getImages2 = function (style) {
+  let call = $.ajax({
+    url: myApp.imageUrl,
+    method: 'GET',
+    dataType: 'json',
+    data: {
+      q: style,
+      has_image: 1,
+      // department: 'Medieval Art',
+      type: 'Painting',
+    }
+  }).then(function (image) {
+
+    // console.log(image);
+    myApp.imagesArray = [];
+    for (let i = 0; i < image.data.length; i++) {
+      // console.log(i);
+      const imagesUrl = image.data[i].images.web.url;
+      myApp.imagesArray.push(imagesUrl);
+      console.log(imagesUrl);
+      console.log(myApp.imagesArray);
+    }
+    for (let i = 0; i < 5; i++) {
+      // console.log(myApp.imagesArray);
+      myApp.appendContent(i);
+    }
+
+    //add pseudo code here
+
+    $('.invisible').remove();
+  })
+}
+
 
 // .fail(function (imageError) {
 //   alert(`Joke content failed: ${imageError.statusText}`);
@@ -60,38 +94,59 @@ myApp.dropDownEventListener = function () {
   $('#style').on('change', function () {
     // console.log($(this).val());
     const chosenStyle = $(this).val();
-    $('.container').empty();
-    myApp.getImages(chosenStyle);
-    console.log(myApp.imagesArray);
-    myApp.appendContent();
+    $('.appendToHere').empty();
+    myApp.getImages2(chosenStyle);
+
+
+    // find how to get to the data in the return
+    // clear array first
+    // put these URLs into the array
+    // use append content with this new array
+
+    // Update Array with new ajax call
+    // console.log(myApp.imagesArray);
+
+
+
+    // FInd a way to make append content find the new array and then append that.
+    // myApp.appendContent();
     $('.currentStyle').text(chosenStyle);
   })
 }
 
 
-//circular menu on/off
-// $('.menu-toggle').click(function () {
-//   $('.menu-toggle').toggleClass('open');
-//   $('.menu-round').toggleClass('open');
-//   $('.menu-line').toggleClass('open');
-// });
+// circular menu on/off
+$('.menu-toggle').click(function () {
+  $('.menu-toggle').toggleClass('open');
+  $('.menu-round').toggleClass('open');
+  $('.menu-line').toggleClass('open');
+});
 
 
 
 // Infinite Scroll Function
-myApp.scrollFunction = function () {
-  $(window).on("scroll", function () {
-    //page height
-    let scrollHeight = $(document).height();
-    //scroll position
-    let scrollPos = $(window).height() + $(window).scrollTop();
-    // fire if the scroll position is 300 pixels above the bottom of the page
-    if (((scrollHeight - 300) >= scrollPos) / scrollHeight == 0) {
-      $('.appendToHere').append(pairToAppend)
-      // console.log('bottom');
-    }
-  });
-}
+
+$(window).scroll(function () {
+  if ($(window).scrollTop() == $(document).height() - $(window).height()) {
+    console.log('bottom');
+  }
+});
+
+
+
+
+
+//   $(window).on("scroll", function () {
+//     //page height
+//     let docHeight = $(document).height();
+//     //scroll position
+//     let windowHeightPlusDistanceToTop = $(window).height() + $(window).scrollTop();
+//     // fire if the scroll position is 300 pixels above the bottom of the page
+//     if (((docHeight - 300) >= windowHeightPlusDistanceToTop) / docHeight == 0) {
+//       console.log('bottom');
+//     }
+//   });
+
 
 // APPEND CONTENT FUNCTION
 myApp.appendContent = function (i) {
@@ -106,16 +161,17 @@ myApp.appendContent = function (i) {
   let imagesToDisplay = myApp.randomizer(myApp.imagesArray);
   // console.log(imagesToDisplay);
   $(`.imageJokeBox${i}`).css('background-image', `url(${imagesToDisplay})`);
-  $('html').delay(5000).animate({
-    scrollTop: $(`.imageJokeBox${i}`).offset().top
-  }, 6000);
+
+  // $('html').delay(5000).animate({
+  //   scrollTop: $(`.imageJokeBox${i}`).offset().top
+  // }, 6000);
 }
 
 // INIT FUNCTION
 myApp.init = function () {
   myApp.dropDownEventListener();
   $.when(myApp.getImages(), myApp.getJokes())
-  .then(function (image, joke) {
+    .then(function (image, joke) {
       // console.log(joke);
 
       // Loop that appends jokes to Joke array when joke AJAX returns
@@ -136,13 +192,19 @@ myApp.init = function () {
       // Call infinte scroll function here 
 
       // APPEND CONTENT LOOP
-      for (let i = 0; i < 30; i++) {
-        // console.log(myApp.imagesArray);
-        myApp.appendContent(i);
-      }
+      // for (let i = 0; i < 30; i++) {
+      //   if (i % 5 != 0) {
+      //     // myApp.appendContent(i);
+      //     console.log(i);
+      //   } else {
+      //     // console.log(i);
 
-      //add pseudo code here
-      
+      //     // myApp.appendContent(i);
+      //   }
+
+      // }
+
+      // pseudo code
       $('.invisible').remove();
     })
 }
@@ -150,5 +212,5 @@ myApp.init = function () {
 // DOC READY 
 $(function () {
   myApp.init()
-  
+
 })
