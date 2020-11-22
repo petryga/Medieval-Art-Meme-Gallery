54a99148f2148538c747431d2520e37573e69df1
Aleksandra Petryga <apetryga@gmail.com>
2020-11-22 17:58:04 -0500

    done with js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ myApp.jokesUrl = `https://icanhazdadjoke.com/search`;
 
 myApp.imagesPortraitsArray = [];
 myApp.imagesWomanArray = [];
-myApp.imagesPeopleArray = [];
+myApp.imagesAnimalArray = [];
 myApp.jokesArray = [];
 
 myApp.limit = 30; //current limit of jokes on the API
@@ -31,6 +31,8 @@ myApp.getPortraitImages = function () {
       for (let i = 0; i < myApp.limit; i++) {
         myApp.imagesPortraitsArray.push(returnObject.data[i].images.web.url);
       }
+    }).fail(function () {
+      alert('Sorry no images, BYE')
     })
 }
 
@@ -45,27 +47,31 @@ myApp.getWomanImages = function () {
       type: 'Painting',
     }
   })
-    .then(function (image) {
+    .then(function (returnObject) {
       for (let i = 0; i < myApp.limit; i++) {
-        myApp.imagesWomanArray.push(image.data[i].images.web.url);
+        myApp.imagesWomanArray.push(returnObject.data[i].images.web.url);
       }
+    }).fail(function () {
+      alert('Sorry no images, BYE')
     })
 }
 
-myApp.getPeopleImages = function () {
+myApp.getAnimalImages = function () {
   return $.ajax({
     url: myApp.imageUrl,
     method: 'GET',
     dataType: 'json',
     data: {
-      q: 'people',
+      q: 'animal',
       has_image: 1,
       type: 'Painting',
     }
-  }).then(function (image) {
+  }).then(function (returnObject) {
     for (let i = 0; i < myApp.limit; i++) {
-      myApp.imagesPeopleArray.push(image.data[i].images.web.url);
+      myApp.imagesAnimalArray.push(returnObject.data[i].images.web.url);
     }
+  }).fail(function () {
+    alert('Sorry no images, BYE')
   })
 }
 
@@ -79,18 +85,14 @@ myApp.getJokes = function () {
       headers: { 'Accept': 'application/JSON' }
     }
   })
-    .then(function (jokeReturn) {
-      for (let i = 0; i < jokeReturn.results.length; i++) {
-        myApp.jokesArray.push(jokeReturn.results[i].joke);
+    .then(function (returnObject) {
+      for (let i = 0; i < returnObject.results.length; i++) {
+        myApp.jokesArray.push(returnObject.results[i].joke);
       }
+    }).fail(function () {
+      alert('Sorry no jokes, BYE')
     })
 }
-
-myApp.randomizer = function (array) {
-  for (var j, x, i = arr.length; i; j = parseInt(Math.random() * i), x = arr[--i], arr[i] = arr[j], arr[j] = x);
-  return arr;
-}
-
 
 // select image array
 myApp.dropDownEventListener = function () {
@@ -102,10 +104,9 @@ myApp.dropDownEventListener = function () {
       selectedArray = myApp.imagesPortraitsArray;
     } else if ($(this).val() == 'woman') {
       selectedArray = myApp.imagesWomanArray;
-    } else if ($(this).val() == 'people') {
-      selectedArray = myApp.imagesPeopleArray;
+    } else if ($(this).val() == 'animal') {
+      selectedArray = myApp.imagesAnimalArray;
     }
-    console.log(selectedArray[1]);
     myApp.appendContent(selectedArray, myApp.jokesArray);
     myApp.scrollFunction(selectedArray, myApp.jokesArray);
   })
@@ -119,7 +120,6 @@ myApp.appendContent = function (chosenArray, jokesArray) {
     </div>
     `;
     $('.append-to-here').append(imageJokeBox);
-    // console.log(chosenArray);
     $(`.image-joke-box${myApp.counter}`).css('background-image', `url('${chosenArray[myApp.counter]}')`);
     myApp.counter++;
   }
@@ -127,40 +127,35 @@ myApp.appendContent = function (chosenArray, jokesArray) {
   myApp.scrollFunction(chosenArray, jokesArray);
 }
 
-console.log("scroll to Top is : ", $(window).scrollTop());
-console.log("the window height is: ", $(window).height());
-console.log("scroll and window = ", ($(window).scrollTop() + $(window).height()));
-console.log("the document height is: ", $(document).height());
-
 myApp.scrollFunction = function (imageArray, jokeArray) {
   $(window).on('scroll', function () {
-    if (Math.round(($(window).scrollTop() + $(window).height()) === $(document).height())) {
+    if (($(window).scrollTop() + $(window).height()) >= ($(document).height() - 3)) {
       if (myApp.counter < myApp.limit) {
         myApp.increment + 5;
         myApp.appendContent(imageArray, jokeArray);
-      }
-      else {
-        alert('We gona send you allllll the way back');
       }
     }
   })
 }
 
+myApp.scrollButton = function () {
+  $('.menu-toggle').on('click', function () {
+    $('html, body').animate({ scrollTop: '0px' }, 300);
+  })
+}
 
 // INIT
 myApp.init = function () {
   myApp.dropDownEventListener();
+  myApp.scrollButton();
   $.when(
     myApp.getPortraitImages(),
     myApp.getWomanImages(),
-    myApp.getPeopleImages(),
+    myApp.getAnimalImages(),
     myApp.getJokes())
-    .done(function (a1, a2, a3, a4) {
-      $('.loader').css('display', 'none')
+    .done(function () {
+      $('.loader').addClass('none')
       $('.after-ajax').css('opacity', '100');
-      // the code here will be executed when all four ajax requests resolve.
-      // a1, a2, a3 and a4 are lists of length 3 containing the response text,
-      // status, and jqXHR object for each of the four ajax calls respectively.
     });
 }
 
