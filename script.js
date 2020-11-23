6170a74ef93ccb66faaa9db9aad4191a67bb000f
Aleksandra Petryga <apetryga@gmail.com>
2020-11-22 22:15:57 -0500

    final version YAY

--- script.js
+++ script.js
@@ -1,21 +1,19 @@
 // namespace
 const myApp = {}
-
+// variable to store chosen art array
 let selectedArray;
-
+// endpoints
 myApp.imageUrl = `https://openaccess-api.clevelandart.org/api/artworks/`;
 myApp.jokesUrl = `https://icanhazdadjoke.com/search`;
-
+// empty arrays to hold ajax returns
 myApp.imagesPortraitsArray = [];
 myApp.imagesWomanArray = [];
 myApp.imagesAnimalArray = [];
 myApp.jokesArray = [];
-
 myApp.limit = 30; //current limit of jokes on the API
-myApp.counter = 0;
-myApp.increment = 5;
-
-// AJAX CALLS
+myApp.counter = 0; // current iteration of loop
+myApp.increment = 5; // number of appends to run when user reaches bottom of screen
+// ajax calls
 myApp.getPortraitImages = function () {
   return $.ajax({
     url: myApp.imageUrl,
@@ -27,15 +25,16 @@ myApp.getPortraitImages = function () {
       type: 'Painting',
     }
   })
+  // push return to array variable called above
     .then(function (returnObject) {
       for (let i = 0; i < myApp.limit; i++) {
         myApp.imagesPortraitsArray.push(returnObject.data[i].images.web.url);
       }
-    }).fail(function () {
+    })
+    .fail(function () {
       alert('Sorry no images, BYE')
     })
 }
-
 myApp.getWomanImages = function () {
   return $.ajax({
     url: myApp.imageUrl,
@@ -51,11 +50,11 @@ myApp.getWomanImages = function () {
       for (let i = 0; i < myApp.limit; i++) {
         myApp.imagesWomanArray.push(returnObject.data[i].images.web.url);
       }
-    }).fail(function () {
+    })
+    .fail(function () {
       alert('Sorry no images, BYE')
     })
 }
-
 myApp.getAnimalImages = function () {
   return $.ajax({
     url: myApp.imageUrl,
@@ -70,11 +69,11 @@ myApp.getAnimalImages = function () {
     for (let i = 0; i < myApp.limit; i++) {
       myApp.imagesAnimalArray.push(returnObject.data[i].images.web.url);
     }
-  }).fail(function () {
+  })
+  .fail(function () {
     alert('Sorry no images, BYE')
   })
 }
-
 myApp.getJokes = function () {
   return $.ajax({
     url: myApp.jokesUrl,
@@ -89,12 +88,15 @@ myApp.getJokes = function () {
       for (let i = 0; i < returnObject.results.length; i++) {
         myApp.jokesArray.push(returnObject.results[i].joke);
       }
-    }).fail(function () {
+    })
+    .fail(function () {
       alert('Sorry no jokes, BYE')
     })
 }
-
-// select image array
+// event listener finds the value of select. 
+// this works in conjunction with a loop in the myApp.appendContent function. 
+// this function assigns the user selected value to the selectedArray variable, and passes selectedArray to the append and scroll functions along with the joke array
+// this function also clears the div where content is appended
 myApp.dropDownEventListener = function () {
   $('#drop-down').on('change', function () {
     myApp.counter = 0;
@@ -111,7 +113,8 @@ myApp.dropDownEventListener = function () {
     myApp.scrollFunction(selectedArray, myApp.jokesArray);
   })
 }
-
+// create a variable that stores the material to be appended that can be looped over
+// append div, change background image to the image url in the image array, and add to the counter. Do while loop
 myApp.appendContent = function (chosenArray, jokesArray) {
   do {
     let imageJokeBox = `
@@ -126,7 +129,8 @@ myApp.appendContent = function (chosenArray, jokesArray) {
   while (myApp.counter < myApp.increment);
   myApp.scrollFunction(chosenArray, jokesArray);
 }
-
+// takes the value of the document and compares it against scroll to top and window height. there is sometimes a small discrepancy of 1-3 pixels, so we factor that into the conditional
+// since the joke API only returns 30, we set that as the limit and use that variable here so that the loop does not break
 myApp.scrollFunction = function (imageArray, jokeArray) {
   $(window).on('scroll', function () {
     if (($(window).scrollTop() + $(window).height()) >= ($(document).height() - 3)) {
@@ -137,14 +141,13 @@ myApp.scrollFunction = function (imageArray, jokeArray) {
     }
   })
 }
-
+// button to bring user back to the top of the page
 myApp.scrollButton = function () {
   $('.menu-toggle').on('click', function () {
     $('html, body').animate({ scrollTop: '0px' }, 300);
   })
 }
-
-// INIT
+// init
 myApp.init = function () {
   myApp.dropDownEventListener();
   myApp.scrollButton();
@@ -158,8 +161,7 @@ myApp.init = function () {
       $('.after-loading').css('opacity', '100');
     });
 }
-
-// DOC READY 
+// doc ready
 $(function () {
   myApp.init();
 })
